cba25d4c8c9821ab9e5e1c1bae0637e35a923603
Redouane Bekkouche <Redouane Bekkouche>
2021-09-06 21:46:13 +0100

    initial commit - sep 06 2021

---
+++ app.js
@@ -0,0 +1,32 @@
+// Access DOM elements
+const reportSection = document.getElementById('response');
+const cityForm = document.getElementById('city-form');
+const cityInput = document.getElementById('city');
+const responseContainer = document.querySelector('#response-container');
+
+// Prepare openweathermap.org request
+let apiRequest = new XMLHttpRequest();
+
+apiRequest.onreadystatechange = () => {
+  if (apiRequest.readyState === 4) {
+    if (apiRequest.status === 404) {
+        return reportSection.textContent = 'City not found, You obviously misspelled the city!';
+    } else {
+    const response = JSON.parse(apiRequest.response);
+    responseContainer.style.display = 'block';
+    reportSection.textContent = 'The weather in ' + response.name + ' is " ' + response.weather[0].main + ' ".';
+    }
+  }
+}
+
+/* 
+ Capture and handle form submit event
+ Prevent default behaviour, prepare and send API request
+*/
+cityForm.addEventListener('submit', ($event) => {
+  $event.preventDefault();
+  const chosenCity = cityInput.value;
+  apiRequest.open('GET', 'https://api.openweathermap.org/data/2.5/weather?q=' + chosenCity + '&APPID=b34fddd3dae4a2eb0ad363b62f98ba1e');
+  apiRequest.send();
+  cityForm.reset();
+});
